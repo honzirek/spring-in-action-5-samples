b85e6d0026d105dbe4532b4821ecaeb0c58d5c7a
mhonzirek <marek.honzirek@xitee.com>
2019-05-20 15:05:28 +0200

    ingredient service/client + zuul proxy

---
+++ ch19/proxy-server/src/main/java/org/honzirek/proxyserver/ProxyServerApplication.java
@@ -0,0 +1,17 @@
+package org.honzirek.proxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServerApplication.class, args);
+	}
+
+}
