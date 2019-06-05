b049bb58da872483f7e414c050f078eb3f58e431
mhonzirek <marek.honzirek@xitee.com>
2019-06-05 16:21:33 +0200

    Spring Boot Admin ... https config

--- ch19/boot-admin-server/src/main/java/tacos/bootadmin/BootAdminServerApplication.java
+++ ch19/boot-admin-server/src/main/java/tacos/bootadmin/BootAdminServerApplication.java
@@ -1,9 +1,23 @@
 package tacos.bootadmin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -12,5 +26,51 @@ public class BootAdminServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootAdminServerApplication.class, args);
 	}
-	
+
+	@Configuration
+	public static class SecurityConfigSBA extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecurityConfigSBA(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+			System.out.println("adminContextPath " + adminContextPath);
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+			successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/assets/**").permitAll()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf()
+
+					.and().csrf().ignoringAntMatchers(
+						adminContextPath + "/instances/**",
+						adminContextPath + "/actuator/**"
+					);
+		}
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+					.inMemoryAuthentication()
+					.withUser("admin")
+					.password("admin")
+					.authorities("ROLE_ADMIN");
+		}
+
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return NoOpPasswordEncoder.getInstance();
+		}
+
+	}
 }
